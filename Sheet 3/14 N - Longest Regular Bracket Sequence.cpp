21ecebc278af2dfb3f776edf31596418c8812b88
jamika78 <ahmed.ibrahim7893@gmail.com>
2024-01-26 16:53:09 +0200

    add n solution

--- Sheet 3/14 N - Longest Regular Bracket Sequence.cpp
+++ Sheet 3/14 N - Longest Regular Bracket Sequence.cpp
@@ -2,7 +2,7 @@
 // صلِ على خير خلق الله محمد
 
 #include <iostream>
-#include <algorithm>
+#include <stack>
 
 using namespace std;
 
@@ -28,13 +28,40 @@ void jamika78_()
 
 void s0lve()
 {
+        string s;
+    cin >> s;
+
+    stack<ll> Stack;
+    Stack.push(-1);
+    ll ans = 0, k = 1;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+            Stack.push(i);
+        else
+        {
+            Stack.pop();
+            if (!Stack.empty())
+            {
+                ll top = i - Stack.top();
+                if (top > ans)
+                    ans = top, k = 1;
+                else if (top == ans)
+                    k++;
+            }
+            else
+                Stack.push(i);
+        }
+    }
+
+    cout << ans << " " << ((ans > 0) ? k : 1) << endl;
 }
 
 signed main()
 {
     jamika78_();
     ll tc = 1;
-    cin >> tc;
+    // cin >> tc;
     for (ll i = 0; i < tc; ++i)
         s0lve();
     return 0;
